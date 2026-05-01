f888f8fa17fabde42c47231e5bc7a22c676b5267
Bilal Ahmad <bilalhassan779966@gmail.com>
2026-05-01 14:05:26 +0500

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-const connectDB = async () =>{
-    await mongoose.connect("mongodb://localhost:27017/internflow")
-    console.log("MongoDB Connected")
-}
-export default connectDB
